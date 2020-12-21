8fa842cf0f6962733b1b6226d0c4e44ef9c5e0e2
Toan Nguyen <toann@zen8labs.com>
2020-12-21 17:21:22 +0700

    Avoid transition interuption because subscription was disposed

--- Sources/RxRouter/TransitionType.swift
+++ Sources/RxRouter/TransitionType.swift
@@ -6,39 +6,25 @@ public protocol TransitionType {
     func asCompletable() -> Completable
 }
 
-public extension TransitionType {
-    func then(_ second: TransitionType) -> TransitionType {
-        return SerialTransition(first: self, second: second)
-    }
-    
-    func and(_ second: TransitionType) -> TransitionType {
-        return ParallelTransition(first: self, second: second)
-    }
-    
-    func delay(_ interval: DispatchTimeInterval) -> TransitionType {
-        return DelayTransition(delay: interval, source: self)
-    }
-}
-
 public struct Transition: TransitionType {
-    public typealias TransitionClosure = (@escaping ()->Void ) throws -> Void
-    private let closure: TransitionClosure
+    public typealias TransitionExecution = (@escaping (Error?)->Void ) -> Void
+    private let execution: TransitionExecution
     
-    public init(closure: @escaping TransitionClosure) {
-        self.closure = closure
+    public init(execution: @escaping TransitionExecution) {
+        self.execution = execution
     }
     
     public func asCompletable() -> Completable {
-        return Completable.create { [closure] callback -> Disposable in
-            do {
-                try closure {
+        return Completable.create { [execution] callback -> Disposable in
+            execution { error in
+                if let error = error {
+                    callback(.error(error))
+                } else {
                     callback(.completed)
                 }
-            } catch {
-                callback(.error(error))
             }
             return Disposables.create()
-        }
+        }.subscribe(on: MainScheduler.instance)
     }
 }
 
@@ -46,85 +32,138 @@ public enum TransitionError: Error {
     case missing(description: String?)
 }
 
+public extension Transition {
+    func then(_ second: Self) -> Self {
+        return Transition { completion in
+            self.execution { firstError in
+                if let error = firstError {
+                    return completion(error)
+                }
+                
+                self.execution { secondError in
+                    completion(secondError)
+                }
+            }
+        }
+    }
+    
+    func and(_ second: Self) -> Self {
+        return Transition { completion in
+            let dispatchGroup = DispatchGroup()
+            var transitionError: Error?
+            
+            dispatchGroup.enter()
+            self.execution { error in
+                transitionError = error
+                dispatchGroup.leave()
+            }
+            
+            dispatchGroup.enter()
+            second.execution { error in
+                transitionError = error
+                dispatchGroup.leave()
+            }
+            
+            dispatchGroup.notify(queue: DispatchQueue.main) {
+                completion(transitionError)
+            }
+        }
+    }
+    
+    func delayStart(_ interval: DispatchTimeInterval) -> Self {
+        return Transition { completion in
+            DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+                self.execution { error in
+                    completion(error)
+                }
+            }
+        }
+    }
+    
+    func delayEnd(_ interval: DispatchTimeInterval) -> Self {
+        return Transition { completion in
+            self.execution { error in
+                DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+                    completion(error)
+                }
+            }
+        }
+    }
+}
 
 public extension Transition {
     static func push(_ controller: ViewControllerConvertible, into nav: UINavigationController?, animated: Bool = true) -> Self {
         Transition { [weak nav] completion in
-            guard let nav = nav else {
-                throw TransitionError.missing(description: "PUSH failed: Missing navigation controller")
+            do {
+                guard let nav = nav else {
+                    throw TransitionError.missing(description: "PUSH failed: Missing navigation controller")
+                }
+                nav.pushViewController(try controller.asViewController(), animated: animated)
+                completion(nil)
+            } catch {
+                completion(error)
             }
-            nav.pushViewController(try controller.asViewController(), animated: animated)
-            completion()
         }
     }
     
     static func present(_ controller: ViewControllerConvertible, from presentingVc: UIViewController?, animated: Bool = true) -> Self {
         Transition { [weak presentingVc] completion in
-            guard let presentingVc = presentingVc else {
-                throw TransitionError.missing(description: "PRESENT failed: Missing presenting view controller")
+            do {
+                guard let presentingVc = presentingVc else {
+                    throw TransitionError.missing(description: "PRESENT failed: Missing presenting view controller")
+                }
+                presentingVc.present(try controller.asViewController(), animated: animated) {
+                    completion(nil)
+                }
+            } catch {
+                completion(error)
             }
-            
-            presentingVc.present(try controller.asViewController(), animated: animated, completion: completion)
         }
     }
     
     static func dismiss(_ controller: UIViewController?, animated: Bool = true) -> Self {
         Transition { [weak controller] completion in
-            guard let controller = controller else {
-                throw TransitionError.missing(description: "DIMISS failed: Missing view controller")
+            do {
+                guard let controller = controller else {
+                    throw TransitionError.missing(description: "DIMISS failed: Missing view controller")
+                }
+                
+                controller.dismiss(animated: animated) {
+                    completion(nil)
+                }
+            } catch {
+                completion(error)
             }
-            
-            controller.dismiss(animated: animated, completion: completion)
         }
     }
     
     static func back(from navController: UINavigationController?, animated: Bool = true) -> Self {
         Transition { [weak navController] completion in
-            guard let navController = navController else {
-                throw TransitionError.missing(description: "BACK failed: Missing navigation controller")
+            do {
+                guard let navController = navController else {
+                    throw TransitionError.missing(description: "BACK failed: Missing navigation controller")
+                }
+                
+                navController.popViewController(animated: animated)
+                completion(nil)
+            } catch {
+                completion(error)
             }
-            
-            navController.popViewController(animated: animated)
-            completion()
         }
     }
     
     static func popToRoot(from navController: UINavigationController?, animated: Bool = true) -> Self {
         Transition { [weak navController] completion in
-            guard let navController = navController else {
-                throw TransitionError.missing(description: "POP-TO-ROOT failed: Missing navigation controller")
+            do {
+                guard let navController = navController else {
+                    throw TransitionError.missing(description: "POP-TO-ROOT failed: Missing navigation controller")
+                }
+                
+                navController.popToRootViewController(animated: animated)
+                completion(nil)
+            } catch {
+                completion(error)
             }
-            
-            navController.popToRootViewController(animated: animated)
-            completion()
         }
-    }
-}
-
-
-struct SerialTransition: TransitionType {
-    let first: TransitionType
-    let second: TransitionType
-    
-    func asCompletable() -> Completable {
-        first.asCompletable().concat(second.asCompletable())
-    }
-}
-
-struct ParallelTransition: TransitionType {
-    let first: TransitionType
-    let second: TransitionType
-    
-    func asCompletable() -> Completable {
-        return Completable.zip(first.asCompletable(), second.asCompletable())
-    }
-}
-
-struct DelayTransition: TransitionType {
-    let delay: DispatchTimeInterval
-    let source: TransitionType
-    
-    func asCompletable() -> Completable {
-        Completable.empty().delay(delay, scheduler: MainScheduler.instance).concat(source.asCompletable())
     }
 }
